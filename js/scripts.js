490719c88ffcfbbd9aa953ff591769b57a065e6b
persepolis1 <electry.ba@gmail.com>
2016-06-24 15:59:00 -0400

    Add Main Banner Container

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+particlesJS.load('particles-js', 'js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
 $(document).ready(function () {
     $('h1').click(function () {
         alert('test');
